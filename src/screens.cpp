41914653a61ec7b52f03115c339d73d40d54f7d0
fumosoft <projectacorn@fumosoft.com>
2024-06-28 21:19:17 +0000

    added frame timing to the screens fsm

--- src/screens.cpp
+++ src/screens.cpp
@@ -1,5 +1,8 @@
 #include "screens.h"
+#include <chrono>
 
+using Duration = std::chrono::duration<float>;
+using namespace std::literals;
 void Screens::process_event(SDL_Event const &event) noexcept {
   std::visit(
       [event, this](auto &&screen) { screen.process_event(event, *this); },
@@ -7,7 +10,34 @@ void Screens::process_event(SDL_Event const &event) noexcept {
 }
 
 void Screens::update([[maybe_unused]] float dt) noexcept {
-  std::visit([dt](auto &&screen) { screen.update(dt); }, current_screen_);
+  static auto previous_frame_time = std::chrono::steady_clock::now();
+  static Duration accumulator{0s};
+  static auto max_dt{.25s};
+  static auto fixed_dt{1s / 100.f};
+
+  std::visit(
+      [&](auto &&screen) {
+        auto const current_frame_time = std::chrono::steady_clock::now();
+        auto const frame_dt =
+            (current_frame_time - previous_frame_time) > max_dt
+                ? max_dt
+                : (current_frame_time - previous_frame_time);
+        previous_frame_time = current_frame_time;
+        accumulator += frame_dt;
+
+        if (accumulator < fixed_dt)
+          return;
+        while (accumulator >= fixed_dt) {
+          screen.update(fixed_dt.count());
+          accumulator -= fixed_dt;
+        }
+
+        // interpolat to accomidate for leftover time in the accumulator
+        // auto const alpha = accumulator / fixed_dt;
+        // std::lerp(ball.x, old_ball.x, alpha);
+        //          update(state, dt);
+      },
+      current_screen_);
 }
 
 void Screens::draw(SDL_Renderer *renderer) const noexcept {
