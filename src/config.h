154296b4e80cb3d93b102315ca020f98f2ec4c4b
fumosoft <projectacorn@fumosoft.com>
2024-06-28 22:20:18 +0000

    updated comments for configuration.h

--- src/config.h
+++ src/config.h
@@ -2,8 +2,20 @@
 #define APP_CONFIG_H_
 
 #include <toml++/toml.h>
+
+/**
+ * Attempts to load configuration data from ./data/config.toml
+ * If the file does not exist, a default configuration is loaded instead.
+ *
+ * \return A hashmap of configuration data.
+ */
 toml::table load_or_create_config();
 
+/**
+ * Gets the default configuration data for the application.
+ *
+ * \return A hashmap of configuration data.
+ */
 toml::table default_config() noexcept;
 
 #endif
