3a3d92fecb8ac53b02cbd3fd28dda459c43d614b
fumosoft <projectacorn@fumosoft.com>
2024-07-31 19:28:52 +0000

    removed old layout

--- libs/math/src/math/shapes/rect.cpp
+++
@@ -1,9 +0,0 @@
-#include <math/shapes/rect.hpp>
-
-using namespace sdltemplate::math::shapes;
-
-bool intersects(Rect const& rhs, Rect const& lhs) noexcept {
-  if(rhs.x > lhs.x + lhs.w || rhs.x + rhs.w < lhs.x) return false;
-  if(rhs.y > lhs.y + lhs.h || rhs.y + rhs.h < lhs.y) return false;
-  return true;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
- #include "sdlapp.h"
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) noexcept {
-  try {
-     SDLApp app{};
-     return app.run();
-//    return SDLApp{}.run();
-  } catch (SDLException const &error) {
-//    spdlog::error(error.what());
-  }
-
-  return 1;
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+  return 0;
 }
