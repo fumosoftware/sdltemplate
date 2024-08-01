f77cd48d24eca8c016363b91031648afff989c30
fumosoft <projectacorn@fumosoft.com>
2024-08-01 23:30:07 +0000

    added collider physics object

---
+++ libs/math/include/math/physics/collider.h
@@ -0,0 +1,8 @@
+#ifndef FUMOSOFT_LIB_MATH_PHYSICS_COLLIDER_H_
+#define FUMOSOFT_LIB_MATH_PHYSICS_COLLIDER_H_
+
+namespace fumo::math::physics {
+
+class Collider {};
+} // namespace fumo::math::physics
+#endif
